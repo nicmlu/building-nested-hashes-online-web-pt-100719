a1af115b61c2e42cca6488a9655cd9019819f75e
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-21 02:59:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,10 +23,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => {"Lord Montague"}, :age => {"53"}
+        :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-         :name => {"Lady Montague"}, :age => {"54"}
+         :name => "Lady Montague", :age => "54"}
         },
       :hero => {
         :name => "Romeo", :age => "15", :status => "alive"
@@ -38,10 +38,10 @@ def third_challenge
         :name => {"Lord Capulet"}, :age => {"50"}
         },
       :matriarch => {
-         :name => {"Lord Capulet"}, :age => {"50"}
+         :name => {"Lady Capulet"}, :age => {"51"}
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet", :age => "15", :status => "alive"
         },
       :heroine_friends => []
    }
