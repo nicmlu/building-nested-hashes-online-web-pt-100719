f03870a6bf9c2c8313b5c8033eae9517220c10f5
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-21 02:56:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,10 +23,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => {"Lord Montague"}, :age => {"53"}
         },
       :matriarch => {
-        #your key/value pairs here
+         :name => {"Lord Montague"}, :age => {"53"}
         },
       :hero => {
         #your key/value pairs here
